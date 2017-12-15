627e86e0c0385c2d0f7822b5f74014455cda4c77
julia sheremet <juliajessicasher@gmail.com>
2017-12-15 13:02:53 -0800

    add loop for multiple choice check box

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,98 @@
 //business logic
-function PizzaSize(size) {
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  // this.pizza = [];
+}
+
+function PizzaOptions(size) {
   this.size = size;
-}
-
-function PizzaToppings(toppings) {
   this.topping = [];
-// this.name = [];
 }
 
-PizzaSize.prototype.pizzaSizePrototype = function() {
-  return this.size;
+// function PizzaToppings(toppings) {
+// }
+
+Contact.prototype.fullName = function () {
+  return this.firstName + " " + this.lastName;
 }
 
-PizzaToppings.prototype.pizzaToppingPrototype = function() {
-  return this.topping;
-
+PizzaOptions.prototype.pizzaOptionsPrototype = function() {
+  return this.size + " " + this.topping;
 }
 
+// PizzaToppings.prototype.pizzaToppingPrototype = function() {
+//   return this.topping;
+//
+// }
 
-// function resetFields () {
-//   $("input#first-name").val("");
+
+function resetFields () {
+  $("input#first-name").val("");
 //   // $("select#pizzaSize").val("");
 //   // $("input#toppings").val("");
-// }
+}
 
 //user interface
 $(document).ready(function() {
- $("form#pizzaForm").submit(function(event) {
-   event.preventDefault();
+  event.preventDefault();
+    $("form#pizzaForm").each(function() {
 
-   var pizzaSizeInput = $("select#pizzaSize").val(""); //might not let me use select
+     var inputtedFirstName = $("input#first-name").val();
+     var inputtedLastName = $("input#last-name").val();
 
-   var newPizzaOrderPt1 = new PizzaSize(pizzaSizeInput);
+     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-   $("#toppingSelection").each(function() {
 
-    var toppingInput = $(this).find("input#toppings").val();
+   ///NEED IT'S OWN FUNCTION
+    $("#toppingSelection").each(function() {
 
-    var newPizzaOrderPt2 = new PizzaTopping(toppingInput);
-    newPizzaOrderPt1.topping.push(newPizzaorderPt2);
+      var pizzaSizeInput = $("select#pizzaSize").val(""); //might not let me use select
+      var toppingInput = $(this).find("input#toppings").val();
+      var newPizzaOrder = new PizzaOptions(pizzaSizeInput, toppingInput);
+      // var newPizzaOrderPt2 = new PizzaTopping();
+      newPizzaOrder.topping.push(newPizzaorderPt2);
   });
 
   $("#toppingSelection").remove(); //removes the users input
-  $("ul#showOrderDetails").append("<li><span class='sendtoHtml'>" + newPizzaOrderPt2.pizzaToppingPrototype() + "<span><li>"); //send toppings to list
+  // $("ul#showOrderDetails").append("<li><span class='sendtoHtml'>" + newPizzaOrderPt2.pizzaToppingPrototype() + "<span><li>"); //send toppings to list
+  $("ul#showOrderDetails").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
 
   $(".sendtoHtml").last().click(function() {
     $(this).hide().slideDown('slow'); //adding style
-    $("#show-order").show();
+    $("#showOrderDetails").show();
+    $("#showOrderDetails h4").text(newContact.firstName);
+    $(".first-name").text(newContact.firstName);
+    $(".last-name").text(newContact.lastName);
     $(".outputSize").text(newPizzaOrderPt1.size);
     $(".outputToppings").text(newPizzaOrderPt2.toppings);
     // newPizzaOrderpt1.topping.forEach(function(topping) {
     // // $("ulToppings").append("<li>" + PizzaToppings.pizzaToppingPrototype() + "</li>");
     // //   });
     // // });
-    // $("input#first-name").val(""); //taking in the input if the name
-
+    $("input#first-name").val(""); //taking in the input if the name
+    $("input#new-last-name").val("");
 )};
 
 
+var checkboxes = document.getElementsByName('employee');////allows you to select multiple checkboxes
+var selected = [];
+for (var i=0; i<checkboxes.length; i++) {
+    if (checkboxes[i].checked == true) {
+        selected.push(checkboxes[i].value + ' ');
+    }
+}
+// if(userAnswer === "Pepperoni")
+//   push.Pepperoni
 
 
 
 
- }); //close submit function
+
+
+
+
+
+
+  }); //close submit function
 }); //close document ready function
