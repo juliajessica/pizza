ea80261a19712cac9903945cacf84fb4240ef280
julia sheremet <juliajessicasher@gmail.com>
2017-12-15 09:55:09 -0800

    add 2 constructors, 2 objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 //business logic
-function Pizza(size, topping) {
+function PizzaSize(size) {
   this.size = size;
-  this.topping = topping;
-  // this.name = [];
 }
 
-Pizza.prototype.pizzaOrder = function () {
-  return this.size + " " + this.topping;
+function PizzaToppings(toppings) {
+  this.topping = [];
+// this.name = [];
 }
+
+PizzaSize.prototype.pizzaOrder = function() {
+  return this.size;
+}
+
+PizzaToppings.prototype.pizzaorder2 = function() {
+  return this.topping;
+}
+
 
 function resetFields () {
   $("select#pizzaSize").val("");
@@ -16,7 +24,26 @@ function resetFields () {
 
 //user interface
 $(document).ready(function() {
+ $("form#pizzaForm").submit(function(event) {
+   event.preventDefault();
+
+   var pizzaSizeInput = $("select#pizzaSize").val("");
+
+   var newPizzaOrderPt1 = new PizzaSize (pizzaSizeInput);
+
+   $("#toppingSelection").each(function() {
+
+    var toppingInput = $(this).find("input#toppings").val();
+
+    var newPizzaOrderPt2 = new PizzaTopping (toppingInput);
+    newPizzaOrderPt1.topping.push(newPizzaorderPt2);
+  });
+
+  $()
 
 
-}
-)
+
+
+
+ }); //close submit function
+}); //close document ready function
