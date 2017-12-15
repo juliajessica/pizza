cba271dfcf3d71a08127233b33d91de9c67a1306
julia sheremet <juliajessicasher@gmail.com>
2017-12-15 13:38:30 -0800

    create new scripts sheet to start over

--- js/scripts1.js
+++ js/scripts1.js
@@ -1,21 +1,15 @@
 //business logic
-function Contact(first, last, order) {
-  this.firstName = first;
-  this.lastName = last;
-  this.order = [];
-}
+// function Contact(first, last, order) {
+//   this.firstName = first;
+//   this.lastName = last;
+//   this.order = [];
+// }
 
 function PizzaOptions(size) {
   this.size = size;
   this.topping = [];
 }
 
-// function PizzaToppings(toppings) {
-// }
-
-// Contact.prototype.fullName = function () {
-//   return this.firstName + " " + this.lastName;
-// }
 
 PizzaOptions.prototype.pizzaOptionsPrototype = function() {
   return this.size + " " + this.topping;
@@ -26,25 +20,10 @@ PizzaOptions.prototype.pizzaOptionsPrototype = function() {
 //
 // }
 
-
-// function resetFields () {
-//   $("input#first-name").val("");
-// //   // $("select#pizzaSize").val("");
-// //   // $("input#toppings").val("");
-// }
-
 //user interface
 $(document).ready(function(event) {
   $("form#pizzaForm").submit(function() {
     event.preventDefault();
-
-    // var inputtedFirstName = $("input#first-name").val();
-    // var inputtedLastName = $("input#last-name").val();
-    //
-    // var newContact = new Contact(inputtedFirstName, inputtedLastName);
-
-   ///NEED IT'S OWN FUNCTION
-    // $("#toppingSelection").each(function() {
 
     var pizzaSizeInput = $("#pizzaSize").val(); //might not let me use select
     var toppingInput = $("input[name=age]:checked").val();
@@ -68,20 +47,12 @@ $(document).ready(function(event) {
 
     $(".sendtoHtml").last().click(function() {
       $(this).hide().slideDown('slow'); //adding style
-      // $("#show-order").show();
-      // $("#show-order h4").text(newContact.firstName);
-      // $(".first-name").text(newContact.firstName);
-      // $(".last-name").text(newContact.lastName);
       $(".outputSize").text(newPizzaOrderPt1.size);
       $(".outputToppings").text(newPizzaOrderPt2.toppings);
     // newPizzaOrderpt1.topping.forEach(function(topping) {
     // // $("ulToppings").append("<li>" + PizzaToppings.pizzaToppingPrototype() + "</li>");
     // //   });
     // // });
-//       $("input#first-name").val(""); //taking in the input if the name
-//       $("input#new-last-name").val("");
-// )};
-
 
 
 
