c78499d653e7a3f3b6420bb354ebce49030fccc8
julia sheremet <juliajessicasher@gmail.com>
2017-12-19 14:57:43 -0800

    update js and place array in UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,56 @@
 //business logic
-var veggie = 10;
-var meat = 12;
-var xlmeat = 15;
+var pizzaBasePrice = 10;
 
-function PizzaOptions(size, topping) {
+function Pizza(size, topping) {
   this.size = size;
   this.topping = topping;
 }
 
-PizzaOptions.prototype.pizzaOptionsPrototype = function() {
-
-  if(this.topping === "Pepperoni" || this.topping === "Meatballs") {
-    var chosenToppings = meat;
-  } else {
-    var chosenToppings = veggie;
+Pizza.prototype.pizzaPrototype = function() {
+  if(this.size == "Small") {
+    var pizzaPrice = pizzaBasePrice;
   }
-  if(this.size === "Family-Size") {
-    var chosenToppings = meat;
-  } else {
-    var chosenToppings = veggie;
+  if(this.size == "Medium") {
+    var pizzaPrice = pizzaBasePrice += 1;
   }
-  if(this.size === "Family-Size" && this.topping === "Pepperoni" || this.size === "Family-Size" && this.topping === "Meatballs") {
-    var chosenToppings = xlmeat;
-  } else {
-    var chosenTopping = veggie;
+  if(this.size == "Large") {
+    var pizzaPrice = pizzaBasePrice += 2;
   }
-  return chosenToppings;
+  if(this.size == "Family-Size") {
+    var pizzaPrice = pizzaBasePrice += 4;
+  }
+  if(this.topping.length > 0) {
+    var pizzaPrice = pizzaBasePrice + (this.topping.length + .75);
+  }
+  else {
+    alert("Please complete your order before you continue!");
+  }
+  return pizzaPrice
 }
 
 //user interface
 $(document).ready(function() {
   $("#pizzaForm").submit(function(event) {
     event.preventDefault();
+    var toppingArray = [];
 
     var pizzaSizeInput = $("#pizzaSize").val();
-    var toppingInput = $("input[name=userToppingChoice]:checked").val();
-    var newPizzaOrder = new PizzaOptions(pizzaSizeInput, toppingInput);
-    var finalPrice = newPizzaOrder.pizzaOptionsPrototype();
+    $("input:checkbox[name=userToppingChoice]:checked").each(function() {
+     var pizzaToppings = $(this).val();
+     toppingArray.push(" " + pizzaToppings);
+
+   });
+
+    var newPizzaOrder = new Pizza(pizzaSizeInput, toppingArray);
+
+    var finalPrice = newPizzaOrder.pizzaPrototype();
     $("ul#showOrderDetails").append("<li><span class='sendtoHtml'>" + newPizzaOrder.size + "<span></li>");
-
-    var orderfinal = newPizzaOrder.pizzaOptionsPrototype();
     $("#showOrderDetails").show();
-    $("#showOrderDetails").append("your price is: " + "$" + orderfinal);
+    $("#showOrderDetails").append("your price is: " + "$" + finalPrice);
 
-    $(".sendtoHtml").last().click(function() {
+    // var orderfinal = newPizzaOrder.pizzaOptionsPrototype();
+
+      $(".sendtoHtml").last().click(function() {
       $("#showOrderDetails").fadeIn();
       $(".outputSize").text(newPizzaOrder.size);
       $(".outputToppings").text(newPizzaOrder.topping);
@@ -51,3 +58,22 @@ $(document).ready(function() {
     });
   });
 });
+
+
+//   if(this.topping === "Pepperoni" || this.topping === "Meatballs") {
+//     var chosenToppings = meat;
+//   } else {
+//     var chosenToppings = veggie;
+//   }
+//   if(this.size === "Family-Size") {
+//     var chosenToppings = meat;
+//   } else {
+//     var chosenToppings = veggie;
+//   }
+//   if(this.size === "Family-Size" && this.topping === "Pepperoni" || this.size === "Family-Size" && this.topping === "Meatballs") {
+//     var chosenToppings = xlmeat;
+//   } else {
+//     var chosenTopping = veggie;
+//   }
+//   return chosenToppings;
+// }
